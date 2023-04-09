00ff2424678275eacae50b55303fc70ed335e78d
Jean-François Laurendeau <laurendeau.jf@outlook.com>
2023-04-09 09:55:37 -0400

    Added ReadOnlySpan and ReadOnlyMemoryProperty.

--- src/Roseau.DateHelpers/OrderedDates.cs
+++ src/Roseau.DateHelpers/OrderedDates.cs
@@ -4,7 +4,7 @@ using Roseau.DateHelpers.DateArrayStrategies;
 
 namespace Roseau.DateHelpers;
 
-public sealed class OrderedDates : IEnumerable<DateOnly>
+public sealed class OrderedDates : IEnumerable<DateOnly>, IEquatable<OrderedDates?>
 {
 	readonly DateOnly[] dates;
 	public OrderedDates(DateOnly[] dates)
@@ -22,13 +22,21 @@ public sealed class OrderedDates : IEnumerable<DateOnly>
 	}
 	public int Count => dates.Length;
 	public DateOnly this[int index] => dates[index];
-	public ReadOnlyCollection<DateOnly> Dates => Array.AsReadOnly(dates);
-	public ReadOnlySpan<DateOnly> AsSpan() => dates.AsSpan();
-	public bool Contains(DateOnly date) => dates.Contains(date);
-	public IEnumerator<DateOnly> GetEnumerator() => Dates.GetEnumerator();
 
+	public ReadOnlySpan<DateOnly> AsSpan() => AsSpan(0);
+	public ReadOnlySpan<DateOnly> AsSpan(int start) => AsSpan(start, dates.Length);
+	public ReadOnlySpan<DateOnly> AsSpan(int start, int length) => new(dates, start, length);
+	public ReadOnlyMemory<DateOnly> AsMemory() => AsMemory(0);
+	public ReadOnlyMemory<DateOnly> AsMemory(int start) => AsMemory(start, dates.Length);
+	public ReadOnlyMemory<DateOnly> AsMemory(int start, int length) => new(dates, start, length);
+
+	public bool Contains(DateOnly date) => 0 <= BinarySearch(date);
+	public int BinarySearch(DateOnly date) => Array.BinarySearch(dates, date);
+
+	public ReadOnlyCollection<DateOnly> AsCollection() => Array.AsReadOnly(dates);
+	public IEnumerator<DateOnly> GetEnumerator() => AsCollection().GetEnumerator();
 	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
+	
 	public static bool IsSorted(DateOnly[] dates)
 	{
 		if (dates is null)
@@ -40,5 +48,27 @@ public sealed class OrderedDates : IEnumerable<DateOnly>
 		while (i <= j && dateAti <= (dateAti = dates[i])) i++;
 		return i > j;
 	}
-
+	
+	public override int GetHashCode() => ((IStructuralEquatable)dates).GetHashCode(EqualityComparer<DateOnly>.Default);
+	public override bool Equals(object? obj)
+	{
+		if (obj is null)
+			return false;
+		if (obj.GetType() != this.GetType())
+			return false;
+		return Equals(obj as OrderedDates);
+	}
+	public bool Equals(OrderedDates? other)
+	{
+		return other is not null &&
+			   ReferenceEquals(this, other);
+	}
+	public static bool operator ==(OrderedDates? left, OrderedDates? right)
+	{
+		return EqualityComparer<OrderedDates>.Default.Equals(left, right);
+	}
+	public static bool operator !=(OrderedDates? left, OrderedDates? right)
+	{
+		return !(left == right);
+	}
 }
